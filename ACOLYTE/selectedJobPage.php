0b9ef638f86e008ce5680de3f3fb8cef1337312c
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-04-23 16:30:24 -0400

    selected job page done. only css left

--- ACOLYTE/selectedJobPage.php
+++ ACOLYTE/selectedJobPage.php
@@ -19,26 +19,26 @@
   if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-    echo "<p>" . $row["title"]. "</p><p>" . $row["locationOfJob"] . "</p><p>"
-    . $row["payment"]. "</p> <p>" . $row["id"]. "</p> ";
+  ?>
+
+    <div id="form-div">
+              <label for="title">TITLE:</label> <br><br>
+              <p><?php echo $row["title"] ?> </p>
+              <label for="email">EMAIL:</label><br><br>
+              <p><?php echo $row["email"] ?></p>           
+              <label for="location">LOCATION:</label><br><br>
+              <p><?php echo $row["locationOfJob"] ?></p>
+              <label for="payment">PAYMENT(USD):</label><br><br>
+              <p><?php echo $row["payment"] ?></p>
+              <label for="description">DESCRIPTION:</label><br><br>
+              <p><?php echo $row["descriptionOfJob"] ?></p>
+    </div>
+    <?php
     }
     } else { echo "0 results"; }
     $conn->close();
-
-       /* 
-        <div id="form-div">
-              <label for="title">TITLE:</label> <br><br>
-              <p></p>
-              <label for="email">EMAIL:</label><br><br>
-              <p></p>           
-              <label for="location">LOCATION:</label><br><br>
-              <p></p>
-              <label for="payment">PAYMENT(USD):</label><br><br>
-              <p></p>
-              <label for="description">DESCRIPTION:</label><br><br>
-              <p></p>
-    </div>
-*/
 ?>
+       
+
 </body>
 </html>
